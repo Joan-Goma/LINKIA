b51238fddd6569883b7b21f12993b92f3ec642bc
Joan-Goma <61229558+Joan-Goma@users.noreply.github.com>
2021-02-05 09:11:32 +0100

    Update Main.java

--- src/m09/joangoma/dev/EJ01/Main.java
+++ src/m09/joangoma/dev/EJ01/Main.java
@@ -3,16 +3,10 @@ package m09.joangoma.dev.EJ01;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void inicio() {
-        int numDNI;
-        Scanner scanner = new Scanner(System.in);       
-        do {
-            System.out.println("Por favor introduce el DNI sin letra, 0 para salir");     
-            numDNI = Integer.parseInt(scanner.nextLine());
-            if(numDNI == 0){ return;}
-            System.out.println(numDNI + calcDNI(numDNI));
-        } while (true);
+    public static void main(String[] args) {
+        int numDNI = 49347661;      
+        System.out.println("Por favor introduce el DNI sin letra, 0 para salir");     
+        System.out.println(numDNI + calcDNI(numDNI));
     }
 
     public static String calcDNI(int DNI){
